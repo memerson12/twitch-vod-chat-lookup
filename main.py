dd054a58555874d784402d1ed8a9a29e28f9f43d
Michael Emerson <emerson.michael12@gmail.com>
2022-03-28 00:57:17 -0400

    Pulling all vod messages

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
-import os
+import requests as requests
 
-from dotenv import load_dotenv
-
-load_dotenv()
+vod_id = 1437510706
+twitch_client_id = 'kimne78kx3ncx6brgo4mv6wki5h1ko'
+user_to_search = 'erynstreams'
 
 
 def main():
-    print(os.getenv('TWITCH_CLIENT_ID'))
+    session = requests.Session()
+    session.headers = {'Client-ID': twitch_client_id, 'Accept': 'application/vnd.twitchtv.v5+json'}
+    response = session.get('https://api.twitch.tv/v5/videos/1437510706/comments', timeout=10)
+    response.raise_for_status()
+    data = response.json()
+    # print(data['comments'][0])
+
+    cursor = None
+    if '_next' in data:
+        cursor = data['_next']
+        # time.sleep(0.1)
+    while cursor:
+        response = session.get(f'https://api.twitch.tv/v5/videos/{vod_id}/comments?cursor={cursor}', timeout=10)
+        response.raise_for_status()
+        data = response.json()
+
+        print(data['comments'][0])
+        if '_next' in data:
+            cursor = data['_next']
+            # time.sleep(0.1)
+        else:
+            cursor = None
 
 
 if __name__ == '__main__':
